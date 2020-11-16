e0345f254b9913a5b63dc74059e3adae26aa3e57
EugenyX_X <38291801+PistrunXvX@users.noreply.github.com>
2020-11-16 22:16:34 +0300

    First comment/ Ready logic component

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Header from './Header.svelte';
+	import About from './AboutUs.svelte';
+	import YouCan from './YouCan.svelte';
+	import YouGet from './YouGet.svelte';
+	import Form from './FeedbackForm.svelte';
+	import Slider from './Slider.svelte';
+	import Price from './Price.svelte';
+	import Advantage from './Advantage.svelte';
+	import Cooperative from './Cooperative.svelte';
+	import Sertificat from './Sertificat.svelte';
+	import Footer from './Footer.svelte';
+</script>
+
+<Header/>
+<About/>
+<YouCan/>
+<Form/>
+<YouGet/>
+<Slider/>
+<Price/>
+<Advantage/>
+<Cooperative/>
+<Sertificat/>
+<Footer/>
+
+<style>
+
+</style>
