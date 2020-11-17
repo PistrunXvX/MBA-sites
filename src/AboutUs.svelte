8f851699aa3805e21151ed82657d8a884c33e7b5
ferismer <54484370+ferismer@users.noreply.github.com>
2020-11-17 15:14:18 +0300

    form

--- src/AboutUs.svelte
+++ src/AboutUs.svelte
@@ -1,4 +1,107 @@
 
+    <div class="aboutUs">
+        <div class="container-fluid">
+        <div class="row justify-content-start">
+            <div class="col-xl-7">
+
+                <div class="about__block about__block-1">
+                    <div class="about__block__title">
+                        <h1 class="title__text">
+                            Что это
+                        </h1>
+                    </div>
+                    <div class="about__block_description ">
+                        <p class="description__text">
+                            Образовательный проект РШУ для компаний с <br> далекоидущими бизнес-планами. Вы сами регулируете
+                            процесс обучения своих сотрудников
+
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <div class="row justify-content-end">
+            <div class="col-xl-8">
+
+                <div class="about__block about__block-2">
+                    <div class="about__block__title">
+                        <h1 class="title__text end">
+                            Для чего 
+                        </h1>
+                    </div>
+                    <div class="about__block_description">
+                        <p class="description__text end">
+                            Вы создаете кадровый резерв для развития компании
+                           Растет результативность каждого сотрудника и компании в целом
+                        Обучение формирует корпоративную культуру и репутацию компании
+                        Вы повышеаете стоимость и статус компании для привлечения инвестиций
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <div class="row justify-content-start">
+            <div class="col-xl-7">
+
+                <div class="about__block about__block-1">
+                    <div class="about__block__title">
+                        <h1 class="title__text">
+                            Для кого
+                        </h1>
+                    </div>
+                    <div class="about__block_description">
+                        <p class="description__text">
+                            Наши услуги ориентированы на собственников, топ-менеджмент, HR-специалистов и руководителей
+функциональных подразделений
+
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <div class="row justify-content-end">
+            <div class="col-xl-6">
+
+                <div class="about__block about__block-4">
+                    <div class="about__block__title">
+                        <h1 class="title__text end">
+                            Выгода
+                        </h1>
+                    </div>
+                    <div class="about__block_description">
+                        <p class="description__text end">
+                            Покупать курсы отдельно не нужно: достаточно выбрать и оплатить подходящий тариф, чтобы
+                            <a href="#" class="des__link">получить доступ к более 1500 обучающих программ</a>
+
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+
+
+
+
+    </div>
+
+</div>
 
 
 
@@ -6,38 +109,99 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<style lang="scss" >
+<style lang="scss">
     @import 'style/theme.scss';
 
+   
+.container-fluid{
+    padding: 0;
+    margin: 0;
+}
+
+
+
+    .aboutUs{
+        color:$darkBlue;
+        font-family: $fbck;
+        padding:78px 100px 61px 100px;
+
+        
+    
+    
+    .about__block{
+        margin-bottom:115px;
+        position: relative;
+       
+        
+
+    }
+
+    .about__block-1::before{
+        position: absolute;
+        content:'';
+        width:5px;
+        height:100%;
+        background-color: $orange;
+        margin-left:-23px;
+        
+        
+        
+        
+        
+    }
+
+
+
+    .about__block-2::after{
+        position: absolute;
+        content:'';
+        width:5px;
+        height:100%;
+        background-color: $darkBlue;
+       
+        margin-left: 102.5%;
+        top: 0;
+        left: 0;
+        
+        
+
+        
+    }
+
+
+    .about__block-4::after{
+        position: absolute;
+        content:'';
+        width:5px;
+        height:100%;
+        background-color: $darkBlue;
+       
+        margin-left: 102.5%;
+        top: 0;
+        left: 0;
+        
+        
+
+        
+    }
+
+    .title__text{
+        font-size:$blockTitleSize;
+        
+        
+
+
+    }
+    .description__text{
+        font-size: $description;
+    }
+
+    .end{
+        text-align: right;
+    }
+            .des__link{
+                color:$orange;
+                text-decoration: none;
+            }
+    }
 </style>
--- src/FeedbackForm.svelte
+++ src/FeedbackForm.svelte
@@ -1,30 +1,175 @@
+ 
+    <div class="form-container">
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-xl-7">
+
+                <div class="text-block">
+
+                    <h2 class="text-block_content">
+                        Всегда важно понять, кто ведёт обучение и как проходят занятия - с радостью пришлём
+                        вам один из модулей
+
+                    </h2>
+
+                </div>
+
+            </div>
+
+
+
+            <div class="col-xl-5">
+                <div class="form-block">
+                    <div class="row">
+                    <div class="col-xl-6">
+
+                    
+                    <input type="text" class="form-control form__name" placeholder="Ваше имя" require>
+                </div>
+                <div class="col-xl-6">
+                    <input type="email" class="form-control form__email" placeholder="Ваш телефон" require>
+                </div>
+                 </div>
+                    <input type="email" class="form-control  form_send__email" placeholder="Ваш электронный адрес"
+                        require>
+                   <label class="label">
+                    <input type="checkbox" class="checkbox">
+                    <span class="fake"></span>
+                    <span class="p">Я согласен с политикой конфиденциальности</span>
+                   </label>
+                   
+                </div>
+                <button class="btn form__btn" type="submit">Отправить</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<style lang="scss" >
+<style lang="scss">
     @import 'style/theme.scss';
+
+
+
+    .form-container{
+        display: flex;
+        align-items:center;
+        background-color:$darkBlue;
+        padding-top:211px;
+        padding-bottom:260px;
+        padding-left:80px;
+        padding-right:80px;
+
+
+        .text-block_content{
+            font-family: $fbck;
+            font-size: 39px;
+            color:$colorWhite;
+        }
+
+
+        .text-block{
+             padding-right:216px;
+            
+        }
+
+
+        .form__name{
+
+            margin-bottom:39px;
+        }
+
+        .form_send__email{
+            margin-bottom:39px;
+        }
+
+
+        .checkbox{
+            margin-bottom: 83px;
+        }
+
+        .form-control{
+            padding:22px 30px;
+            background-color:$darkBlue;
+           
+            border: 4px solid $colorWhite;
+           
+            font-size: 25px;
+
+        }
+
+        .form-control::placeholder{
+            color:#D3D3D3;
+        }
+
+        .form-control::placeholder{
+            color:#colorWhite;
+        }
+
+        .form-control:focus{
+            color:$colorWhite;
+        }
+
+
+
+
+            .form-block{
+
+                .p{
+                    color:$colorWhite;
+                    font-size: $formInputSize;
+                }
+            }
+
+
+        
+           
+            
+            .label{}
+            
+            .checkbox{
+                display:none;
+            }
+
+            .fake{
+                display:inline-block;
+                width:40px;
+                height:40px;
+                border:5px solid $colorWhite;
+                position: relative;
+                
+
+            }
+
+            .fake::before{
+                content:'';
+                position: absolute;
+                display:block;
+                background-image:url('../img/Vector 9.svg');
+                background-size: 20px, 20px;
+                background-repeat:no-repeat;
+                background-position:center;
+                
+               
+                transform:translate(-50%, -50%);
+                opacity:0;
+                transition: .2s;
+            }
+            
+            
+            .checkbox:checked + .fake::before{
+                opacity:1;
+            }
+
+    }
+
+    
+
+    
+
+
 
 </style>
