a0b18e15a0f985454bd555836b3340741cfbd9c4
EugenyX_X <38291801+PistrunXvX@users.noreply.github.com>
2020-11-18 16:43:08 +0300

    1 stage Done

--- src/Cooperative.svelte
+++ src/Cooperative.svelte
@@ -1,9 +1,38 @@
-<div class="container-fluid">
+<div class="container-fluid coop__main">
     <h2>Мы сотрудничаем</h2>
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="cooperative__container">
+                <ul>
+                    <li><img src="/img/cooperative_img_1.png" alt=""></li>
+                    <li><img src="/img/cooperative_img_2.png" alt=""></li>
+                    <li><img src="/img/cooperative_img_3.png" alt=""></li>
+                    <li><img src="/img/cooperative_img_4.png" alt=""></li>
+                </ul>
+            </div>
+        </div>
+    </div>
     <div class="row">
         <div class="col">
             <div class="cooperative__container">
-                
+                <ul>
+                    <li><img src="/img/cooperative_img_5.png" alt=""></li>
+                    <li><img src="/img/cooperative_img_6.png" alt=""></li>
+                    <li><img src="/img/cooperative_img_7.png" alt=""></li>
+                    <li><img src="/img/cooperative_img_8.png" alt=""></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="cooperative__container">
+                <ul>
+                    <li><img src="/img/cooperative_img_9.png" alt=""></li>
+                    <li><img src="/img/cooperative_img_10.png" alt=""></li>
+                    <li><img src="/img/cooperative_img_11.png" alt=""></li>
+                    <li><img src="/img/cooperative_img_12.png" alt=""></li>
+                </ul>
             </div>
         </div>
     </div>
@@ -11,5 +40,34 @@
 
 <style lang="scss" >
     @import 'style/theme.scss';
+
+    .coop__main{
+        
+        h2{
+            font-size: $blockTitleSize;
+            font-family: $fbck;
+            color: $darkBlue;
+
+            text-transform: uppercase;
+            text-align: center;
+
+            margin-top: 5%;
+            margin-bottom: 5%;
+        }
+    }
+
+    .cooperative__container{
+
+        li{
+            display: inline;
+            list-style: none;
+
+            margin-right: 10%;
+        }
+
+        img{
+            width: 150px;
+        }
+    }
 
 </style>
--- src/Sertificat.svelte
+++ src/Sertificat.svelte
@@ -1,25 +1,74 @@
-<div class="container-fluid">
-    <h2>Лицензии</h2>
-    <div class="row">
-        <div class="col-sm-12 col-md-12 col-lg-4">
-            <div class="first__sertificat">
-                <img src="img/sertificat_img_1.jpg" alt="Лицензия на право ведения образовательной деятельности">
+<div class="container-fluid sertificat__container">
+    <!-- <div class="container"> -->
+        <h2>Лицензии</h2>
+        <div class="row justify-content-start">
+            <div class="col-sm-12 col-md-12 col-lg-4">
+                <div class="first__sertificat">
+                    <a href="https://uprav.ru/about/#lg=1&slide=15"><img src="img/sertificat_img_1.jpg" class="img-fluid mx-auto d-block" alt="Лицензия на право ведения образовательной деятельности"></a>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-4">
+                <div class="second__sertificat">
+                    <a href="https://uprav.ru/about/#lg=1&slide=0"><img src="img/sertificat_img_2.jpg" class="img-fluid mx-auto d-block" alt="Сертификат соответсвия"></a>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-4">
+                <div class="first__sertificat">
+                    <a href="https://uprav.ru/about/#lg=1&slide=1"><img src="img/sertificat_img_3.jpg" class="img-fluid mx-auto d-block" alt="Сертификат соответсвия"></a>
+                </div>
             </div>
         </div>
-        <div class="col-sm-12 col-md-12 col-lg-4">
-            <div class="second__sertificat">
-                <img src="img/sertificat_img_2.jpg" alt="Сертификат соответсвия">
-            </div>
-        </div>
-        <div class="col-sm-12 col-md-12 col-lg-4">
-            <div class="first__sertificat">
-                <img src="img/sertificat_img_1.jpg" alt="Сертификат соответсвия">
-            </div>
-        </div>
-    </div>
+    <!-- </div> -->
 </div>
 
 <style lang="scss" >
     @import 'style/theme.scss';
+
+    .sertificat__container{
+
+        background-color: $grey;
+
+        padding-bottom: 5%;
+        
+        h2{
+            font-size: $blockTitleSize;
+            font-family: $fbck;
+            color: $darkBlue;
+
+            text-align: center;
+            text-transform: uppercase;
+
+            padding-top: 5%;
+        }
+
+        img{
+            padding-top: 10%;
+
+            animation: 0.3s ease-out animation__img__back;
+        }
+        
+        img:hover{
+            animation: 0.3s ease-in animation__img__to;
+            transform: scale(1.1, 1.1);
+        }
+    }
+
+    @keyframes animation__img__to{
+        from{
+            transform: scale(1,1);
+        }
+        to{
+            transform: scale(1.1, 1.1);
+        }
+    }
+
+    @keyframes animation__img__back{
+        from{
+            transform: scale(1.1,1.1);
+        }
+        to{
+            transform: scale(1, 1);
+        }
+    }
 
 </style>
