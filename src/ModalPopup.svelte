cafb69ab27e4c26bfd2dcc4366ecc777078009be
ferismer <54484370+ferismer@users.noreply.github.com>
2020-11-24 22:02:44 +0300

    !

--- src/ModalPopup.svelte
+++ src/ModalPopup.svelte
@@ -1,40 +1,4 @@
-<script>
-    window.addEventListener("DOMContentLoaded", function() {
-    function setCursorPosition(pos, elem) {
-        elem.focus();
-        if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
-        else if (elem.createTextRange) {
-            let range = elem.createTextRange();
-            range.collapse(true);
-            range.moveEnd("character", pos);
-            range.moveStart("character", pos);
-            range.select()
-        }
-    }
-    
-    function mask(event) {
-        let matrix = "+7 (___) ___ __ __",
-            i = 0,
-            def = matrix.replace(/\D/g, ""),
-            val = this.value.replace(/\D/g, "");
-        if (def.length >= val.length) val = def;
-        this.value = matrix.replace(/./g, function(a) {
-            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
-        });
-        if (event.type == "blur") {
-            if (this.value.length == 2) this.value = ""
-        } else setCursorPosition(this.value.length, this)
-    };
-        let input = document.querySelector("#tel3");
-        input.addEventListener("input", mask, false);
-        input.addEventListener("focus", mask, false);
-        input.addEventListener("blur", mask, false);
-    });
 
-    // /Маска телефона
-    
-        
-      </script>
 
 <div class="modal fade" id="modal__header" tabindex="-1" aria-labelledby="modal__header__label" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
@@ -57,7 +21,7 @@
                     </div>
                     <div class="row">
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-                            <input type="tel" id="tel3" placeholder="Ваш номер телефона" required>
+                            <input type="tel" id="tel" placeholder="Ваш номер телефона" required>
                         </div>
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
                             <input type="email" placeholder="Ваша электронная почта" required>
